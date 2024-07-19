23eaddd19d605aac771a3701691bd26fb40df9b4
Tesfa Tesfaye <108762211+tesfatesfaye@users.noreply.github.com>
2024-07-19 18:01:08 -0400

    Update vite.config.ts
    Improved the vite.config.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(),react()],
+  plugins: [svgr(),react(),tsconfigPaths()],
+   server: {
+    open: true, // This will open the browser automatically
+  },
 })
